3abe11de3b518a5faa5e08455963ed167334c58a
Mann Patel <mann.patel.1207@gmail.com>
2026-03-31 18:34:42 +0530

    feat: add Strapi migration scripts and implement provider-whitelist plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,25 @@
-export default () => ({
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
   preview: {
     enabled: false,
   },
+  'provider-whitelist': {
+    enabled: true,
+    resolve: './src/plugins/provider-whitelist',
+  },
 });
+
